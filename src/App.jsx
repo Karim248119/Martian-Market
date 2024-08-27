0f59693ccc76bfe24b07372379574cda7c921c75
Karim248119 <karimoda66@gmail.com>
2024-08-27 13:50:33 +0300

    checkout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 
 function App() {
+  useEffect(() => {
+    document.title = "Martian Market";
+  }, []);
+
   return (
     <>
       <Outlet />
