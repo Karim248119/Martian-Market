17fab104e0f1bd265c77fff12a0376a1a15cc146
Karim248119 <karimoda66@gmail.com>
2024-08-27 14:06:50 +0300

    Updates

---
+++ static/js/394.4bda0588.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunke_store=self.webpackChunke_store||[]).push([[394],{5394:(e,t,r)=>{r.d(t,{HSs:()=>n});var a=r(3441);function n(e){return(0,a.k5)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{fill:"none",strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"32",d:"M304 336v40a40 40 0 0 1-40 40H104a40 40 0 0 1-40-40V136a40 40 0 0 1 40-40h152c22.09 0 48 17.91 48 40v40m64 160 80-80-80-80m-192 80h256"},child:[]}]})(e)}}}]);
+//# sourceMappingURL=394.4bda0588.chunk.js.map
